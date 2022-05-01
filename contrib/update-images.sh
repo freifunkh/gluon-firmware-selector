b2bde6caab3f81d77ed5dbc5f68c4309121d12a6
Tom Herbers <mail@tomherbers.de>
2022-05-01 03:23:20 +0200

    contrib/update-images.sh: download multiple branches and other dir

--- contrib/update-images.sh
+++ contrib/update-images.sh
@@ -1,10 +1,15 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-FACTORY="https://firmware.darmstadt.freifunk.net/images/stable/factory/"
-SYSUPGRADE="https://firmware.darmstadt.freifunk.net/images/stable/sysupgrade/"
+set -e
 
-mkdir images
+for BRANCH in "stable" "beta" "testing";
+do
+	echo "updating ${BRANCH}"
 
-curl -o images/gluon-factory-example.html "$FACTORY"
-curl -o images/gluon-sysupgrade-example.html "$SYSUPGRADE"
+	mkdir -p images/${BRANCH}
 
+	curl -sSfo images/${BRANCH}/gluon-factory-example.html "https://firmware.darmstadt.freifunk.net/images/${BRANCH}/factory/"
+	curl -sSfo images/${BRANCH}/gluon-other-example.html "https://firmware.darmstadt.freifunk.net/images/${BRANCH}/other/"
+	curl -sSfo images/${BRANCH}/gluon-sysupgrade-example.html "https://firmware.darmstadt.freifunk.net/images/${BRANCH}/sysupgrade/"
+
+done
